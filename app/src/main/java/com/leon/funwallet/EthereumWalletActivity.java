978af9d43f9d141677d6c40cab01acad8ba8cc31
leon <526247082@qq.com>
2018-08-24 17:28:33 +0800

    Add send eth ui.

--- app/src/main/java/com/leon/funwallet/EthereumWalletActivity.java
+++ app/src/main/java/com/leon/funwallet/EthereumWalletActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -83,4 +84,7 @@ public class EthereumWalletActivity extends AppCompatActivity {
         });
     }
 
+    public void onSendEth(View view) {
+
+    }
 }
