88e640e0fb05a9f99bf63ccebcd60e0ef68133ed
leon <526247082@qq.com>
2018-08-24 15:58:22 +0800

    Get eth wallet balance.

--- app/src/main/java/com/leon/funwallet/EthWalletManager.java
+++ app/src/main/java/com/leon/funwallet/EthWalletManager.java
@@ -3,20 +3,22 @@ package com.leon.funwallet;
 
 import android.content.Context;
 import android.content.ContextWrapper;
-import android.util.Log;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.ECKeyPair;
+import org.web3j.crypto.Keys;
 import org.web3j.crypto.Wallet;
-import org.web3j.crypto.WalletUtils;
+import org.web3j.crypto.WalletFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -24,14 +26,18 @@ public class EthWalletManager {
 
     private static final String TAG = "EthWalletManager";
 
-    private Wallet wallet;
+    private WalletFile wallet;
+
 
     private static final String PASSWORD = "a12345678";
 
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     private final Executor getWalletExecutor = Executors.newSingleThreadExecutor();
 
     private static EthWalletManager sEthWalletManager;
+
+
 
     private EthWalletManager() {
     }
@@ -48,14 +54,28 @@ public class EthWalletManager {
         return sEthWalletManager;
     }
 
-    public void loadWallet(final ContextWrapper contextWrapper) {
+    public void loadWallet(final ContextWrapper contextWrapper, final OnWalletLoadedListener listener) {
+        if (wallet != null && listener != null) {
+            listener.onWalletLoaded(wallet);
+        }
         getWalletExecutor.execute(new Runnable() {
             @Override
             public void run() {
                 try {
                     File walletDir = contextWrapper.getDir("eth", Context.MODE_PRIVATE);
-                    String fileName = WalletUtils.generateLightNewWalletFile(PASSWORD, walletDir);
-                    Log.d(TAG, "fileName: " + walletDir);
+                    if (walletDir.exists()) {
+                        File[] files = walletDir.listFiles();
+                        wallet = objectMapper.readValue(files[0], WalletFile.class);
+                    } else {
+                        ECKeyPair ecKeyPair = Keys.createEcKeyPair();
+                        wallet  = Wallet.createLight(PASSWORD, ecKeyPair);
+                        String walletFileName = getWalletFileName(wallet);
+                        File destination = new File(walletDir, walletFileName);
+                        objectMapper.writeValue(destination, wallet);
+                    }
+                    if (listener != null) {
+                        listener.onWalletLoaded(wallet);
+                    }
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
                 } catch (NoSuchProviderException e) {
@@ -72,7 +92,15 @@ public class EthWalletManager {
     }
 
 
+    private static String getWalletFileName(WalletFile walletFile) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("'UTC--'yyyy-MM-dd'T'HH-mm-ss.SSS'--'");
+        return dateFormat.format(new Date()) + walletFile.getAddress() + ".json";
+    }
+
+
+
+
     public static interface OnWalletLoadedListener {
-        void onWalletLoaded(Wallet wallet);
+        void onWalletLoaded(WalletFile wallet);
     }
 }
--- app/src/main/java/com/leon/funwallet/EthereumWalletActivity.java
+++ app/src/main/java/com/leon/funwallet/EthereumWalletActivity.java
@@ -3,18 +3,68 @@ package com.leon.funwallet;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.web3j.crypto.WalletFile;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+import java.math.BigInteger;
 
 
-public class EthereumWalletActivity extends AppCompatActivity{
+public class EthereumWalletActivity extends AppCompatActivity {
+
+    private static final String TAG = "EthereumWalletActivity";
+
+    private WalletFile mWalletFile;
+
+    private TextView mWalletAddressText;
+
+    private static final String HEX_PREFIX = "0x";
+
+    private TextView mBalanceText;
+
+    private Web3j mWeb3j = Web3jFactory.build(new HttpService("https://ropsten.infura.io/1UoO4I/"));
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ethereum_wallet);
+        mWalletAddressText = findViewById(R.id.address);
+        mBalanceText = findViewById(R.id.balance);
+
+        EthWalletManager.getInstance().loadWallet(this, new EthWalletManager.OnWalletLoadedListener() {
+            @Override
+            public void onWalletLoaded(WalletFile w) {
+                mWalletFile = w;
+                Log.d(TAG, "onWalletLoaded: " + mWalletFile.getAddress().length());
+                final String address = HEX_PREFIX + mWalletFile.getAddress();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mWalletAddressText.setText(address);
+                    }
+                });
+
+
+                try {
+                    final BigInteger balance = mWeb3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send().getBalance();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mBalanceText.setText(String.valueOf(balance.intValue()));
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
-    public void onCreateWallet(View view) {
-        EthWalletManager.getInstance().loadWallet(this);
-    }
 }
